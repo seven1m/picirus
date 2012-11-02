24a37d5a319285848b12fc2c408f6591a50233c9
Tim Morgan <tim@timmorgan.org>
2012-11-02 18:51:26 -0500

    Apply styling to popover on-demand.

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,14 @@
 $(function() {
+  // a hack to apply an extra class to the popover
+  with($.fn.popover.Constructor) {
+    prototype.realSetContent = prototype.setContent;
+    prototype.setContent = function() {
+      this.realSetContent();
+      if(this.tip().is(':has(.popover-title:contains(error))')) {
+        this.tip().addClass('popover-error');
+      }
+    };
+  }
   $('[rel=popover]').popover();
   $('[rel=popover-static]').popover('show');
-  $('.popover:has(.popover-title:contains(error))').addClass('popover-error')
 });
